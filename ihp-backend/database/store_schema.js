e2c0b52a02f5f35a0cfd90c27cb732bd705364db
dongmin lee <pkalsh345@gmail.com>
2020-11-22 13:45:59 +0900

    매장 스키마 수정

--- ihp-backend/database/store_schema.js
+++ ihp-backend/database/store_schema.js
@@ -5,29 +5,51 @@
  * @author pkalsh
  */
 
-var Schema = {};
+const mongoose = require('mongoose');
 
-Schema.createSchema = function(mongoose) {
+const { Schema } = mongoose;
+const StoreSchema = new Schema({
+	name: {
+		type: String,
+		index: 'hashed', 
+		'default':''
+	},
+	address: {
+		type: String,
+		'default':''
+	},
+	tel: {
+		type: String, 
+		'default':''
+	},
+	geometry: {
+		'type': {type: String, 'default': "Point"},
+		coordinates: [{type: "Number"}]
+	},
+	created_at: {
+		type: Date, 
+		index: {unique: false}, 
+		'default': Date.now
+	},
+	updated_at: {
+		type: Date, 
+		index: {unique: false}, 
+		'default': Date.now
+	}
+});
+
+StoreSchema.index({geometry:'2dsphere'});
 	
-	// 스키마 정의
-	var StoreSchema = mongoose.Schema({
-	    name: {type: String, index: 'hashed', 'default':''},
-	    address: {type: String, 'default':''},
-	    tel: {type: String, 'default':''},
-	    geometry: {
-	    	'type': {type: String, 'default': "Point"},
-	    	coordinates: [{type: "Number"}]
-	    },
-	    created_at: {type: Date, index: {unique: false}, 'default': Date.now},
-	    updated_at: {type: Date, index: {unique: false}, 'default': Date.now}
-	});
-	
-	StoreSchema.index({geometry:'2dsphere'});
+StoreSchema.path('geometry').validate((geometry) => {
+	return geometry.length;
+}, 'geometry 칼럼의 값이 없습니다.');
 
-	// static으로 매장 데이터와 관련한 메서드 정의
+// static으로 매장 데이터와 관련한 메서드 정의
+StoreSchema.statics = {
 
-	return StoreSchema;
-};
+}
+
+module.exports = mongoose.model('store', StoreSchema)
 
 module.exports = Schema;
 

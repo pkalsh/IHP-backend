d7a304f13d279121a4c50ef82f606b4e167887b7
eziong <44022551+eziong@users.noreply.github.com>
2020-11-22 17:40:32 +0900

    goods information 11/22
    promise 적용
    sendGoodInfo로 수정

--- ihp-backend/routes/goods.js
+++ ihp-backend/routes/goods.js
@@ -3,7 +3,9 @@ var convert = require('xml-js');
 var SERVICE_KEY = '=95yvl4Q2Y6DIGgpJ8C%2FdqYE%2FmSENoHJJPwqJv38n%2F8%2BdEBsQ4juKoFov5RbSMne0uY3Z7lSQmlNQ84rTk4dywQ%3D%3D'
 var url = 'http://openapi.price.go.kr/openApiImpl/ProductPriceInfoService/getProductInfoSvc.do';
 
-
+/*
+server.js 에서 사용법 : goods.sendGoodInfo(goods.getGoodId('query'))
+*/
 
 var makeJson = function(body){
     var xml = convert.xml2json(body,{compact:false, spaces:4});
@@ -19,24 +21,37 @@ var makeJson = function(body){
         "goodTotalCnt":xml.elements[0].elements[0].elements[0].elements[7].elements[0].text,
         "goodTotalDivCode":xml.elements[0].elements[0].elements[0].elements[8].elements[0].text
     }
-    //console.log(myjson);
-    myjson = JSON.stringify(myjson);
     return myjson;
+}
+
+var getGoodId = function(query){
+    /* 들어온 쿼리에서 goodId를 분리  -> goodId 생성*/
+    var goodId = 15; // 임의 배정
+    return goodId;
+}
+var sendGoodInfo = function(goodId){    
+    getGoodInfo(goodId)
+    .then(function(goodJson){
+        console.log('goodJson입니다.'+JSON.stringify(goodJson));
+    })
 }
 
 var getGoodInfo = function(goodId){
     var queryParams = '?' + encodeURIComponent('ServiceKey') + SERVICE_KEY; /* Service Key*/
-    queryParams += '&' + encodeURIComponent('goodId') + '=' + encodeURIComponent(goodId); /* */
+        queryParams += '&' + encodeURIComponent('goodId') + '=' + encodeURIComponent(goodId); /* */
     var goodJson;
 
-    request({
-        url: url + queryParams,
-        method: 'GET'
-    }, function (error, response, body) {
-        goodJson = makeJson(body);
-        console.log(goodJson);
-    });
-    return goodJson;
+    return new Promise(function(resolve,reject){
+        request({
+            url: url + queryParams,
+            method: 'GET'
+        }, function (error, response, body) {
+            goodJson = makeJson(body);
+            resolve(goodJson);
+        });
+    })
 }
 
-module.exports.getGoodInfo = getGoodInfo;
+
+module.exports.sendGoodInfo = sendGoodInfo;
+module.exports.getGoodId = getGoodId;

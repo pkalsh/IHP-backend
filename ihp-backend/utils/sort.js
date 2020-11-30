9eba4bc54c714f4806d0a3da334131e418ca1263
eziong <44022551+eziong@users.noreply.github.com>
2020-11-30 20:53:42 +0900

    sort 구현

--- ihp-backend/utils/sort.js
+++ ihp-backend/utils/sort.js
@@ -2,15 +2,19 @@ function sortPath(criteria,everyPath){
     everyPath.sort(function(a,b){
         switch(criteria){
             case 'payment':
-                return a.payment < b.payment ? -1 : a.payment > b.payment ? 1 : 0;
+                return a[0].payment < b[0].payment ? -1 : a[0].payment > b[0].payment ? 1 : 0;
             case 'totalDistance':
-                return a.totalDistance < b.totalDistance ? -1 : a.totalDistance > b.totalDistance ? 1 : 0;
+                return a[0].totalDistance < b[0].totalDistance ? -1 : a[0].totalDistance > b[0].totalDistance ? 1 : 0;
             case 'totalTime':
-                return a.totalTime < b.totalTime ? -1 : a.totalTime > b.totalTime ? 1 : 0;
+                return a[0].totalTime < b[0].totalTime ? -1 : a[0].totalTime > b[0].totalTime ? 1 : 0;
             case 'totalWalk':
-                return a.totalWalk < b.totalWalk ? -1 : a.totalWalk > b.totalWalk ? 1 : 0;
+                return a[0].totalWalk < b[0].totalWalk ? -1 : a[0].totalWalk > b[0].totalWalk ? 1 : 0;
             case 'totalWalkTime':
-                return a.totalWalkTime < b.totalWalkTime ? -1 : a.totalWalkTime > b.totalWalkTime ? 1 : 0;
+                return a[0].totalWalkTime < b[0].totalWalkTime ? -1 : a[0].totalWalkTime > b[0].totalWalkTime ? 1 : 0;
+            case 'price':
+                return a[1] < b[1] ? -1 : a[1] > b[1] ? 1 : 0;
+            case 'total_price':
+                return a[1]+a[0].payment < b[1]+b[0].payment ? -1 : a[1]+a[0].payment > b[1]+b[0].payment ? 1 : 0;
         }
         
     });
